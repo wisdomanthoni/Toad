801b3b1b336d066ed21c21cd945da3b2c2b00726
Wisdom Anthony <wisdomanthoni@gmail.com>
2018-06-05 13:10:16 +0100

    Update app.php

--- app.php
+++ app.php
@@ -1,5 +1,5 @@
 <?php 
 
-define('KEY','VqiYRADw054J3ZZeRigyPG0w8');
-define('SECRET', 'yS1FqYtxLi5FKzVi9MjOufhFRBrwOGbxm3GzCgSMpl37bVQwsx');
+define('KEY','xxxxxxxxxxxxxxxxxxxxx');
+define('SECRET', 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
  ?>
